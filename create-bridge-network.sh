3db0309559b38aad9de68dde5cacc0418b8cbb44
maykinayki <maykinaykii@gmail.com>
2024-02-17 16:34:40 +0100

    create bridge and slave

--- create-bridge-network.sh
+++ create-bridge-network.sh
@@ -1,7 +1,7 @@
 BRIDGE_NAME=lanbr0
 
-PHYSICAL_DEV_IF_NAME=wlo1
-PHYSICAL_DEV_UUID='90255e66-8eaa-4dd3-a02d-4df2b071d8ce'
+PHYSICAL_DEV_IF_NAME=enp56s0u1u1
+PHYSICAL_DEV_UUID='1988e66b-540e-3fcf-aa6f-2e5f6e5afe01'
 
 sudo nmcli connection add con-name $BRIDGE_NAME ifname $BRIDGE_NAME type bridge ipv4.method auto ipv6.method disabled connection.autoconnect yes stp no
 
